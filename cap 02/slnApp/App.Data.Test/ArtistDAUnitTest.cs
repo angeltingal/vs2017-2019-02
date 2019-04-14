4366b3399e39c7779f079bf77acd86f022b583bd
EXTENSION <angeltingal@gmail.com>
2019-04-14 12:06:06 -0500

    Agregando el proyecto 1

---
+++ cap 02/slnApp/App.Data.Test/ArtistDAUnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Data.Test
+{
+    [TestClass]
+    public class ArtistDAUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var da = new ArtistDA();
+            Assert.IsTrue(da.GetCount() > 0);
+        }
+    }
+}
---
+++ cap 02/slnApp/App.Data.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("App.Data.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("App.Data.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("55fb3516-c3c0-4848-a551-645c82df4789")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
